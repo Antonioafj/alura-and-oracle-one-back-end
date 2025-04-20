c79188bf660cfc95c498beb1bee9e5a452243574
Antonioafj <antonioafj.edu@gmail.com>
2025-04-20 01:53:32 -0400

    feito exercicio de fatorial
    Signed-off-by: Antonioafj <antonioafj.edu@gmail.com>

--- exer_2_repeticao/src/Fatorial.java
+++ exer_2_repeticao/src/Fatorial.java
@@ -3,7 +3,17 @@ import java.util.Scanner;
 public class Fatorial {
 
     public static void main(String[] args) {
-        
+        Scanner entrada = new Scanner(System.in);
+        int fatorial = 1 ;
+
+        System.out.println("Digite o numero do fatorial");
+        int numero = entrada.nextInt();
+
+        for (int i = 1; i <= numero; i++) {
+            fatorial *= i;
+        }
+
+        System.out.println("O fatorial é " + fatorial);
     }
 
 }
